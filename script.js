cf3597f46e98bc000503503ffcb956ca612fc2b2
hrz07 <hridoy.developer@yahoo.com>
2022-02-17 12:32:51 +0600

    error handling message done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// initially set balance to 0
+
 let balance = 0;
 
+// a function for take input
 
 function inputFun(id) {
   let input = document.getElementById(id);
@@ -8,30 +11,39 @@ function inputFun(id) {
   return inputNumber;
 }
 
+// Event-Listener for calculate button
+
 document.getElementById("calculate-btn").addEventListener("click", () => {
   let income = inputFun("inputIncome");
   let foodCost = inputFun("foodInput");
   let rentCost = inputFun("rentCost");
   let clothCost = inputFun("clothCost");
 
-
     if(isNaN(income) || isNaN(foodCost) || isNaN(rentCost) || isNaN(clothCost)){
-        console.log('wrong number');
+
+        document.getElementById('numberVarify').style.display = 'block';
+
     }else if(income<=0 || foodCost<0 || rentCost<0 || clothCost<0){
-        console.log('must be gether than 0')
+
+        document.getElementById('positiveVarify').style.display = 'block';
     }  
     else{
         let totalExpenses = foodCost + rentCost + clothCost;
         balance = income - totalExpenses;
         if(totalExpenses > income){
-            console.log('total cost can not be bigger')
+            document.getElementById('incomeVarify').style.display = 'block';
         }else{
             document.getElementById("totalExpenses").innerText = totalExpenses;
             document.getElementById("balance").innerText = balance;
+
+            document.getElementById('numberVarify').style.display = 'none';
+            document.getElementById('positiveVarify').style.display = 'none';
+            document.getElementById('incomeVarify').style.display = 'none';
         }  
     }
- 
 });
+
+// Event-Listener for save button
 
 document.getElementById('saveBtn').addEventListener('click',()=>{
     let income = inputFun("inputIncome");
@@ -43,6 +55,6 @@ document.getElementById('saveBtn').addEventListener('click',()=>{
         document.getElementById('savingAmount').innerText = savingAmount;
         document.getElementById('remainingBalance').innerText = remainingBalance;
     }else{
-        console.log('dhurrrrr')
+        document.getElementById('saveVarify').style.display = 'block';
     }  
 })
